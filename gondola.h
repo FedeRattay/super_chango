82cf2c7049cbf7d413feb5568e5618585ed57e31
Rattay <federattay@gmail.com>
2019-04-26 14:21:20 -0300

    Arregle Codigo de barras en gondola y Agregue el metodo buscar por codigo.

--- gondola.h
+++ gondola.h
@@ -9,7 +9,7 @@ class Gondola: public Contenedor
 	public:
 		void cargar_productos(string nombre_archivo);
 		void cargar_un_producto(Producto producto_nuevo);
-		void buscar_producto_codigo(unsigned long long codigo_producto);
+		void buscar_producto_codigo(string codigo_producto);
 		void modificar_precio(string nombre_producto, double precio_nuevo);
 		void ver_ofertas();
 		~Gondola();
