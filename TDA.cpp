440648764baa44ac89f14c421dafb3e89e502d12
Rattay <federattay@gmail.com>
2019-04-27 23:37:33 -0300

    TDA Nuevo

--- TDA.cpp
+++ TDA.cpp
@@ -1,40 +1,59 @@
-T. D. A.
-
 ----------------------------------
-Producto: //2 Constructores: 1 sin parametros y otro con los 4 atributos(Cuando carga desde el .txt).
-	-nombre: string
-	-codigo: unsigned long long //Porque tenia que ser de 13 digitos
-	-precio: float
-	-oferta: bool
-	
-	+set_nombre(nom)
-		...etc
-	+get_nombre()
-		...etc
+..........	........  	..........
+..........	..........	...   ....
+   ....   	...   ....	...   ....
+   ....   	...   ....	...   ....
+   ....   	...   ....	..........
+   ....   	...   ....	..........
+   ....   	...   ....	...   ....
+   ....   	..........	...   ....
+   ....   	........  	...   ....
 ----------------------------------
-//////////////////////////////////
-----------------------------------
-Contenedor: //CLASE PADRE //1 Constructor sin parametro que inicializa todo en nulo.
-	-tamanio: int
-	-contenido[]: producto
-	
-	+ver_contenido()
-	+buscar_producto_nombre(nombre_prod) // * Reciben sus parametros del metodo buscar producto.
-	+quitar_producto(nombre_prod) *
-----------------------------------
-//////////////////////////////////
-----------------------------------
-Gondola: //REPOSITOR
-	
-	+cargar_producto(tamanio, nombre_txt) //Carga desde el .txt & Recibe un tamanio = cantidad de lineas del archivo desde una funcion en el main.
-	+buscar_producto_codigo(cod_prod)
-	+modificar_precio(nombre_prod,precio_nuevo) *
-	+ver_ofertas()
+Producto:
+		-nombre: string;
+		-codigo: string;
+		-precio: double;
+		-oferta: bool;
+		
+		+Producto();
+		+Producto(string,string,double,bool);
+		+void asignar_nombre(string);
+		+void asignar_codigo_barra(string);
+		+void asignar_precio(double);
+		+void asignar_oferta(bool);
+		+string obtener_nombre();
+		+string obtener_codigo_barra();
+		+double obtener_precio();
+		+bool obtener_oferta();
+		+void operator=(Producto);
 ----------------------------------
 //////////////////////////////////
 ----------------------------------
-Chango: //CLIENTE
-	
-	+calcular_total() //Devuelve total con ofertas consideradas.
-	+cargar_producto(producto_elegido) *// 50 MAX. Usa el auxiliar para expandir su tamanio.
+Contenedor:
+		:-:tamanio: int;
+		:-:espacio_ocupado: int;
+		
+		+Producto *contenido;
+		+void mostrar_producto(Producto);
+		+void redimensionar(int);
+		+Contenedor();
+		+void ver_contenido();
+		+void buscar_producto_nombre(string);
+		+virtual void agregar_producto(Producto);
+		+void quitar_producto(string);
+		+~Contenedor();
+----------------------------------
+//////////////////////////////////
+----------------------------------
+Contenedor>>Gondola: //REPOSITOR
+		+void cargar_productos(string);
+		+void cargar_un_producto(Producto);
+		+void buscar_producto_codigo(string);
+		+void modificar_precio(string,double);
+		+void ver_ofertas();
+----------------------------------
+//////////////////////////////////
+----------------------------------
+Contenedor>>Chango: //CLIENTE
+		+void calcular_total();
 ----------------------------------
