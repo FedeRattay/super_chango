0f1e195f820b8b479703c4766d1190d9afad08f9
Rattay <federattay@gmail.com>
2019-04-27 21:07:59 -0300

    quitar no redimensiona & agregar redimensiona mejor

--- contenedor.cpp
+++ contenedor.cpp
@@ -42,7 +42,6 @@ void Contenedor::redimensionar(int nuevo_tamanio)
 	Producto *aux = new Producto[nuevo_tamanio];
 	if (nuevo_tamanio > 0)
 	{
-		//guarda en el producto en un auxiliar
 		if (tamanio <= nuevo_tamanio)
 		{
 			for (int i = 0; i < tamanio; i++)
@@ -57,28 +56,31 @@ void Contenedor::redimensionar(int nuevo_tamanio)
 				aux[i] = contenido[i];
 			}
 		}
-		tamanio = nuevo_tamanio;
 		delete[] contenido;
 		contenido = aux;
+		tamanio = nuevo_tamanio;
 	}
 }
 
 void Contenedor::agregar_producto(Producto un_producto)
 {
 	int tam_new = 0;
-	if (tamanio > 0)
-		tam_new = tamanio + 1;
-	else
-		tam_new = 1;
+	if(tamanio == 0)
+		tam_new = 2;
 
-	int ultima_pos = tamanio;
+	int ultima_pos = espacio_ocupado-1;
 
 	string nombre_new = un_producto.obtener_nombre();
 	string codigo_new = un_producto.obtener_codigo_barra();
 	double precio_new = un_producto.obtener_precio();
 	bool oferta_new = un_producto.obtener_oferta();
-
-	redimensionar(tam_new);
+	
+	espacio_ocupado++;
+	if(espacio_ocupado == tamanio)
+	{
+		tam_new = espacio_ocupado+5;
+		redimensionar(tam_new);
+	}
 
 	contenido[ultima_pos].asignar_nombre(nombre_new);
 	contenido[ultima_pos].asignar_codigo_barra(codigo_new);
@@ -90,26 +92,18 @@ void Contenedor::quitar_producto(string nombre_producto)
 {
 	int pos_eliminar = 0;
 	bool encontrado = false;
-	int nuevo_tamanio = --tamanio;
-
-	Producto aux;
 
 	while ((pos_eliminar < tamanio) && (!encontrado))
 	{
 		if (contenido[pos_eliminar].obtener_nombre() == nombre_producto)
 		{
 			encontrado = true;
+			contenido[pos_eliminar] = contenido[tamanio-1];
+			espacio_ocupado--;
 		}
 		else
 			pos_eliminar++;
 	}
-	if (encontrado)
-	{
-		aux = contenido[pos_eliminar];
-		contenido[pos_eliminar] = contenido[tamanio];
-		contenido[tamanio] = aux;
-	}
-	redimensionar(nuevo_tamanio);
 }
 
 Contenedor::~Contenedor()
